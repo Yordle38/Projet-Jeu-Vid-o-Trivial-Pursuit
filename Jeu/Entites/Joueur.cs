ff8e3e49b3b27a7915bed513e1081d2b63e5b202
Yordle38 <97458919+Yordle38@users.noreply.github.com>
2024-12-08 09:38:03 +0100

    Getters, Setters et modif constructeur

--- Jeu/Entites/Joueur.cs
+++ Jeu/Entites/Joueur.cs
@@ -21,7 +21,7 @@ public class Joueur : Sprite
         _case = caseD; // case de départ
         _score = 0;
         _jokers = new List<Joker>();
-        Actif = true; // innactifs à la création
+        Actif = false; // innactifs à la création
     }
 
     public void update()
@@ -51,6 +51,39 @@ public class Joueur : Sprite
             }
         }
     }
+
+    public string GetNom()
+    {
+        return _nom;
+    }
+    public int GetScore()
+    {
+        return _score;
+    }
+    
+    public Case GetCase()
+    {
+        return _case;
+    }
+    
+    // Modifie la case du joueur et le place à la position de la case
+    public void SetCase(Case nouvelleCase)
+    {
+        _case = nouvelleCase;
+        _position = nouvelleCase.GetPosition();
+        SetPosition(new Vector2(_position.X+40,_position.Y+40));
+    }
+
+    public void SetPosition(Vector2 position)
+    {
+        _position = position;
+    }
+    
+    public Vector2 GetPosition()
+    {
+        return _position;
+    }
+
 
     public new void Draw(SpriteBatch spriteBatch)
     {
