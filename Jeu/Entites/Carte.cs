83fd21a41f7897facdddca741dad18190c9ba8f0
Yordle38 <97458919+Yordle38@users.noreply.github.com>
2024-12-11 19:34:04 +0100

    ajout classe carte

---
+++ Jeu/Entites/Carte.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using Trivial_Pursuit.Jeu.Enumeration;
+
+namespace Trivial_Pursuit.Jeu.Entites;
+
+public class Carte
+{
+    public Categorie Categorie { get; private set; }
+    private string _question;
+    private List<Reponse> Reponses { get; set; }
+    private Difficulte _difficulte;
+
+    public Carte(Categorie categorie, string question, List<Reponse> reponses, Difficulte difficulte)
+    {
+        Categorie = categorie;
+        _question = question;
+        Reponses = reponses;
+        _difficulte = difficulte;
+    }
+
+    public string GetQuestion()
+    {
+        return _question;
+    }
+    public Difficulte GetDifficulte()
+    {
+        return _difficulte;
+    }
+}
