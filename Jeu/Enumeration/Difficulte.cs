f777d555de18c3e773fe9503e5971ff497f954dd
Yordle38 <97458919+Yordle38@users.noreply.github.com>
2024-12-11 19:42:23 +0100

    ajout difficulte de question et etats du joueur

---
+++ Jeu/Enumeration/Difficulte.cs
@@ -0,0 +1,8 @@
+namespace Trivial_Pursuit.Jeu.Enumeration;
+
+public enum Difficulte
+{
+    Facile,
+    Moyen,
+    Difficile
+}
---
+++ Jeu/Enumeration/EtatJoueur.cs
@@ -0,0 +1,8 @@
+namespace Trivial_Pursuit.Jeu.Enumeration;
+
+public enum EtatJoueur
+{
+    Normal,
+    ChoixDifficulte,
+    ChoixReponse
+}
