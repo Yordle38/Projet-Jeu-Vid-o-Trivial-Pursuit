abe6d6fe1e4ab779ddda903ca1ebdeac33f5f845
Yordle38 <97458919+Yordle38@users.noreply.github.com>
2024-12-12 19:28:31 +0100

    booléen est correcte en public

--- Jeu/Entites/Reponse.cs
+++ Jeu/Entites/Reponse.cs
@@ -3,11 +3,10 @@ namespace Trivial_Pursuit.Jeu.Entites;
 public class Reponse
 {
     public string Texte { get; private set; }
-    private bool _correct;
-
+    public bool EstCorrecte { get; private set; }
     public Reponse(string texte, bool correct)
     {
         Texte = texte;
-        _correct = correct;
+        EstCorrecte = correct;
     }
 }
