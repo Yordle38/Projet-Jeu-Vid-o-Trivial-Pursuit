394d292c9533e92f5b6e84321510961025b73976
Yordle38 <97458919+Yordle38@users.noreply.github.com>
2024-12-14 13:28:01 +0100

    confirmation avant de jouer tour et traitement case chance

--- Jeu/Enumeration/EtatJoueur.cs
+++ Jeu/Enumeration/EtatJoueur.cs
@@ -3,6 +3,8 @@ namespace Trivial_Pursuit.Jeu.Enumeration;
 public enum EtatJoueur
 {
     Normal,
+    AttenteConfirmation,
     ChoixDifficulte,
-    ChoixReponse
+    ChoixReponse,
+    Rejouer
 }
