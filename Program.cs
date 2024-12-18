9a17bbdf09a1ac8da449c730dbf359342ef0b59d
l3miage-sadikim <mossabsadiki04@gmail.com>
2024-12-18 22:43:48 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,113 +1,68 @@
-/*﻿using var game = new Trivial_Pursuit.Game1();
-game.Run();*/
-
-using System;
-using System.Xml.Serialization;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using TrivialPursuit.chap;
 
-namespace TrivialPursuit
+namespace TrivialPursuit.chap
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var trivialPursuit = new TrivialPursuit
-            {
-                Plateau = new Plateau
-                {
-                    Background = new Background { Texture = "Background.png" },
-                    Cases = new List<Case>
-                    {
-                        new Case
-                        {
-                            Type = Enum.TYPECASE.QUESTION,
-                            Couleur = Enum.Couleur.BLEU,
-                            Sprite = new Sprite { Type = "Sprite.png", PositionX = 15, PositionY = 10, Size = 20 },
-                            Joker = new Joker { Id = 1, Nom = "50/50", Effet = "50/50" }
-                        },
-                        new Case
-                        {
-                            Type = Enum.TYPECASE.CHALLENGE,
-                            Couleur = Enum.Couleur.ROUGE,
-                            Sprite = new Sprite { Type = "Sprite.png", PositionX = 10, PositionY = 15, Size = 20 },
-                            Joker = new Joker { Id = 1, Nom = "Relance question", Effet = "Relance question" }
-                        }
-                    }
-                },
-                Categories = new List<Categorie>
-                {
-                    new Categorie
-                    {
-                        Id = 1,
-                        Nom = "Histoire",
-                        Couleur = Enum.Couleur.JAUNE,
-                        Cartes = new List<Carte>
-                        {
-                            new Carte
-                            {
-                                Id = 1,
-                                Difficulte = Enum.Difficulte.Facile,
-                                Question = "Qui a découvert l'Amérique en 1492 ?",
-                                Responses = new List<Reponse>
-                                {
-                                    new Reponse { Id = 1, Texte = "Christophe Colomb", Correct = true },
-                                    new Reponse { Id = 2, Texte = "Amerigo Vespucci", Correct = false },
-                                    new Reponse { Id = 3, Texte = "Ferdinand Magellan", Correct = false },
-                                    new Reponse { Id = 4, Texte = "Vasco de Gama", Correct = false },
-                                }
-                            }
-                        }
-                    }
-                },
-                Joueurs = new List<Joueur>
-                {
-                    new Joueur
-                    {
-                        Id = 1,
-                        Nom = "Joueur 1",
-                        Score = 10,
-                        Pion = new Pion
-                            { Nom = "Pion 1", ImageLink = "Pion 1.png", Description = "pion bleu", Case = "Case1" },
-                        Jokers = new List<Joker>
-                        {
-                            new Joker { Id = 1, Nom = "50/50", Effet = "50/50" }
-                        }
-                    },
-                    new Joueur
-                    {
-                        Id = 2,
-                        Nom = "Joueur 2",
-                        Score = 5,
-                        Pion = new Pion
-                        {
-                            Nom = "Pion 2", ImageLink = "Pion 2.png", Description = "pion rouge", Case = "Case1"
-                        },
-                        Jokers = new List<Joker>
-                        {
-                            new Joker { Id = 2, Nom = "relance question", Effet = "relance question" }
-                        }
-                    }
-                }
-            };
-            string filePath = @"..\..\xml\trivialpursuit.xml";
-            XMLUtils.Serialization(trivialPursuit, filePath);
-            Console.WriteLine($"Serialization reussie dans le fichier xml : {filePath}");
-            
-            
-        }
-    }
 
-    public static class XMLUtils
-    {
-        public static void Serialization<T>(T obj, string filePath)
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(T));
-            using (var writer = new StreamWriter(filePath))
-            {
-                serializer.Serialize(writer, obj);
-            }
+            Sprite sprite1 = new Sprite("a", 7, 10, 20,4,Enum.Couleur.BLEU);
+            Sprite sprite2 = new Sprite("case1_texture.png", 13, 10, 20,4,Enum.Couleur.BLEU);
+            Sprite sprite3 = new Sprite("case2_texture.png", 10, 15, 25,4,Enum.Couleur.BLEU);
+
+            Case case1 = new Case(Enum.TYPECASE.VIDE, Enum.Couleur.BLEU, sprite1);
+            Case case2 = new Case(Enum.TYPECASE.VIDE, Enum.Couleur.BLEU, sprite2);
+            Case case3 = new Case(Enum.TYPECASE.QUESTION, Enum.Couleur.ROUGE, sprite3);
+
+            Background background = new Background("plateau_texture.png");
+
+            Cases c = new Cases(new List<Case>() { case1, case2, case3 });
+            Plateau plateau = new Plateau(background, c);
+
+            Reponse reponse1 = new Reponse(1, "Louis-Napoléon Bonaparte", true);
+            Reponse reponse2 = new Reponse(2, "Gaston Doumergue", false);
+            Reponses r = new Reponses(new List<Reponse>() { reponse1, reponse2 });
+            Carte carte1 = new Carte(101, Enum.Difficulte.Facile, "Le premier président de la France ?", r);
+            Cartes cartesList1 = new Cartes(new List<Carte>() { carte1 });
+            Categorie categorie1 = new Categorie(1, "Histoire", Enum.Couleur.ROUGE, cartesList1);
+            Categories categories = new Categories(new List<Categorie>() { categorie1 });
+            Joker joker1 = new Joker(1, "50/50", "Supprimer deux mauvaises réponses");
+            Jokers jokersList1 = new Jokers(new List<Joker>() { joker1 });
+
+            Pion pion1 = new Pion("pion1", "pion1.png", "pion bleu", 1);
+            Joueur joueur1 = new Joueur(1, "Bob", 4, pion1, jokersList1);
+            Joueurs js = new Joueurs(new List<Joueur>() { joueur1 });
+            string shema = "http://www.univ-grenoble-alpes.fr/l3miage/trivialpursuit ../xsd/game.xsd";
+            TrivialPursuit t = new TrivialPursuit(plateau, categories, js,shema);
+
+            // Serialisation 
+            string filePath = Path.Combine("..", "..", "..", "xml", "trivialpursuit.xml");
+            XMLUtils.Serialization(t, filePath);
+            Console.WriteLine($"Fichier XML généré avec succès : {filePath}");
+
+            // Désérialisation
+            TrivialPursuit loadedGame = XMLUtils.Deserialization<TrivialPursuit>(filePath);
+            Console.WriteLine("Fichier XML désérialisé avec succès !");
+            
+            string path= Path.Combine("..", "..", "..", "xml", "game.xml");
+            Dom_Xpath xpath = new Dom_Xpath(path);
+            int jo=xpath.CountElemnts("//tp:Joueurs//tp:Joueur");
+            Console.WriteLine($"le nombre de joueur {jo}");
+            xpath.CaseVide(path);
+            int co=xpath.CountElemnts("//tp:Categories//tp:Categorie");
+            Console.WriteLine($"le nombre de Categorie {co}");
+            string pathC= Path.Combine("..", "..", "..", "xml", "questionDesCategories.xml"); 
+            Dom_Xpath xpath1 = new Dom_Xpath(pathC);
+            xpath1.AfficherQuestionsHistoire(pathC);
+            
         }
     }
 }
+ 
+
+
 
